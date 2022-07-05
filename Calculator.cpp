897ebfcb0ed86c328a49f017c15d6090f2f11891
Tobie Rathbun <tsrathbun@gmail.com>
2022-07-05 11:28:21 -0700

    added multiplication

--- Calculator.cpp
+++ Calculator.cpp
@@ -6,5 +6,20 @@ int main() {
     cout << "Enter first number: ";
     cin >> firstNumber;
 
+    string operatorVar;
+    cout << "Enter the operator: ";
+    cin >> operatorVar;
+
+    float secondNumber;
+    cout << "Enter second number: ";
+    cin >> secondNumber;
+
+    float result;
+    if (operatorVar == "x" || operatorVar == "x") {
+        result = firstNumber * secondNumber;
+    }
+
+    cout << result;
+
     return 0;
 }
